220477750cb283a7b38473a5510e18199cb065c1
kamron9 <kamronalimov80@gmail.com>
2025-02-19 14:58:25 +0500

    bug fixes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,15 +1,21 @@
-import { Router } from 'express';
+import { Router } from 'express'
 
-import { createOrder,deleteOrder,getAllOrders,getOrderById,updateOrder } from '../controllers/orderController.js';
+import {
+	createOrder,
+	deleteOrder,
+	getAllOrders,
+	getOrderById,
+	updateOrder,
+} from '../controllers/orderController.js'
 
-import { verifyToken } from '../middleware/authMiddleware.js';
+import { verifyToken } from '../middleware/authMiddleware.js'
 
-const router = Router();
+const router = Router()
 
-router.get('/orders', verifyToken, getAllOrders);
-router.get('/orders/:id', verifyToken, getOrderById);
-router.post('/orders',verifyToken, createOrder);
-router.put('/orders/:id', verifyToken, updateOrder);
-router.delete('/orders/:id', verifyToken, deleteOrder);
+router.get('/orders', verifyToken, getAllOrders)
+router.get('/orders/:id', verifyToken, getOrderById)
+router.post('/orders', createOrder)
+router.put('/orders/:id', verifyToken, updateOrder)
+router.delete('/orders/:id', verifyToken, deleteOrder)
 
-export default router;
+export default router
