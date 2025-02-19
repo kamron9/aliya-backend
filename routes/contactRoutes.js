240d3eff4dd42a6a244dd6f7784db9f304a3e142
kamron9 <kamronalimov80@gmail.com>
2025-02-19 14:00:23 +0500

    bug fixes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,13 +1,19 @@
-import {createContact,deleteContact,getContact,getContacts,updateContact}from '../controllers/contactController.js';
-import { Router } from 'express';
-import {verifyToken} from '../middleware/authMiddleware.js';
+import { Router } from 'express'
+import {
+	createContact,
+	deleteContact,
+	getContact,
+	getContacts,
+	updateContact,
+} from '../controllers/contactController.js'
+import { verifyToken } from '../middleware/authMiddleware.js'
 
-const router = Router();
+const router = Router()
 
-router.post('/contact',verifyToken, createContact);
-router.get('/contact',verifyToken, getContacts);	
-router.get('/contact/:id',verifyToken, getContact);
-router.put('/contact/:id',verifyToken, updateContact);
-router.delete('/contact/:id',verifyToken, deleteContact);
+router.post('/contact', createContact)
+router.get('/contact', verifyToken, getContacts)
+router.get('/contact/:id', verifyToken, getContact)
+router.put('/contact/:id', verifyToken, updateContact)
+router.delete('/contact/:id', verifyToken, deleteContact)
 
-export default router;
+export default router
